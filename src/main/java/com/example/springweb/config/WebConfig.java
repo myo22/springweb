bdf4f409ee2c7f62125ecfeb7cd46e14e8546e9d
minhyung <dlalsgud12@naver.com>
2023-07-20 16:48:25 +0900

    ViewResolver

--- src/main/java/com/example/springweb/config/WebConfig.java
+++ src/main/java/com/example/springweb/config/WebConfig.java
@@ -1,11 +1,13 @@
 package com.example.springweb.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.awt.*;
 
@@ -25,5 +27,18 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         System.out.println("configureDefaultServletHandling이 실행됩니다.");
         configurer.enable(); // 디폴트 서블릿을 사용하도록 설정.
+    }
+
+    // 스프링은 ViewResolver를 설정하라. JSP를 사용하려면 InternalResourceViewResolver가 필요하다.
+    // InternalResourceViewResolver를 Bean으로 등록하라.
+    // /WEB-INF/view/ + "hello" + ".jsp" <- 포워딩
+    // jsp는 url을 통하여 직접 접근할 수 없다.
+    // Controller를 통해서만 접근할 수 있도록 한다.
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        return  resolver;
     }
 }
--- src/main/java/com/example/springweb/controller/HelloController.java
+++ src/main/java/com/example/springweb/controller/HelloController.java
@@ -3,19 +3,31 @@ package com.example.springweb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello") // /hello 요청이 오면 해당 메소드를 실행하라.
     // 이해를 돕기위해 서블릿과 유사하게 만든거지 스프링에서 이렇게 사용하지 않는다.
-    public void hello(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println("Hello Spring MVC");
-        out.close();
+    public String hello() throws IOException {
+        // 로직을 수행.
+        return "hello"; // hello.jsp가 되기를 원한다. -> hello.jsp로 포워딩.
     }
 }
+
+// URL을 처리하는 컨트롤러의 메소드가 hello를 리턴하면 hello.jsp로 포워딩하고 싶다.
+
+//javax.servlet.ServletException: Circular view path [hello]: would dispatch back to the current handler URL [/hello] again. Check your ViewResolver setup! (Hint: This may be the result of an unspecified view, due to default view name generation.)
+//	org.springframework.web.servlet.view.InternalResourceView.prepareForRendering(InternalResourceView.java:210)
+//	org.springframework.web.servlet.view.InternalResourceView.renderMergedOutputModel(InternalResourceView.java:148)
+//	org.springframework.web.servlet.view.AbstractView.render(AbstractView.java:316)
+//	org.springframework.web.servlet.DispatcherServlet.render(DispatcherServlet.java:1405)
+//	org.springframework.web.servlet.DispatcherServlet.processDispatchResult(DispatcherServlet.java:1149)
+//	org.springframework.web.servlet.DispatcherServlet.doDispatch(DispatcherServlet.java:1088)
+//	org.springframework.web.servlet.DispatcherServlet.doService(DispatcherServlet.java:964)
+//	org.springframework.web.servlet.FrameworkServlet.processRequest(FrameworkServlet.java:1006)
+//	org.springframework.web.servlet.FrameworkServlet.doGet(FrameworkServlet.java:898)
+//	javax.servlet.http.HttpServlet.service(HttpServlet.java:489)
+//	org.springframework.web.servlet.FrameworkServlet.service(FrameworkServlet.java:883)
+//	javax.servlet.http.HttpServlet.service(HttpServlet.java:583)
+//	org.apache.tomcat.websocket.server.WsFilter.doFilter(WsFilter.java:51)
